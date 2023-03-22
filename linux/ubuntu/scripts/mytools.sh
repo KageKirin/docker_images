5eff4c8659e4312374eb7d515e92c0353587c08c
Chris Helmich <kagekirin@gmail.com>
2023-03-22 21:51:20 +0100

    refactor (mytools.sh): install 'dotnet' to '/opt/hostedtoolcache/dotnet'

--- linux/ubuntu/scripts/mytools.sh
+++ linux/ubuntu/scripts/mytools.sh
@@ -92,19 +92,24 @@ zig version
 printf "\n\t🐋 Installed Zig 🐋\t\n"
 
 printf "\n\t🐋 Installing .NET 🐋\t\n"
-#curl -LO https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh
-#cat ./dotnet-install.sh
-#bash ./dotnet-install.sh --install-dir /dotnet --no-path --channel STS
-#bash ./dotnet-install.sh --install-dir /dotnet --no-path --channel LTS
-#rm ./dotnet-install.sh
-#export PATH=$PATH:/dotnet
-#export DOTNET_ROOT="$(dirname $(which dotnet))"
-#echo $PATH
+# prerequisites for native build
+# note: change libicu70 -> libicu71 as soon as available
+apt-get install libc6  libgcc1  libgcc-s1  libgssapi-krb5-2  libicu-dev  libicu70  liblttng-ust1  libssl3  libstdc++6  libunwind8  zlib1g
 
-apt-get install -y dotnet-sdk-7.0 dotnet-sdk-6.0 dotnet-runtime-7.0 dotnet-runtime-6.0
+curl -LO https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh
+bash ./dotnet-install.sh --install-dir /opt/hostedtoolcache/dotnet --no-path --channel STS  # net 7.0
+bash ./dotnet-install.sh --install-dir /opt/hostedtoolcache/dotnet --no-path --channel LTS  # net 6.0
+rm ./dotnet-install.sh
+export DOTNET_ROOT=/opt/hostedtoolcache/dotnet
+export PATH=$PATH:$DOTNET_ROOT
+{
+  echo "DOTNET_ROOT=${DOTNET_ROOT}"
+  echo "PATH=\$PATH:\$DOTNET_ROOT"
+} | tee -a /etc/environment
 
 which dotnet
 dotnet --version
 dotnet --info
-dotnet --list-sdks
-dotnet --list-runtimes
+#dotnet --list-sdks
+#dotnet --list-runtimes
+printf "\n\t🐋 Installed .NET 🐋\t\n"
