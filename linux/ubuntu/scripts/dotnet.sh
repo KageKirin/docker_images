c428b02ce570770b94999e77b511fbd9bf4881db
Chris Helmich <kagekirin@gmail.com>
2023-03-23 16:49:53 +0100

    refactor (dotnet.sh): do not apt-get-install libs for native C# builds
    reason: not needed atm

--- linux/ubuntu/scripts/dotnet.sh
+++ linux/ubuntu/scripts/dotnet.sh
@@ -9,7 +9,7 @@
 printf "\n\t🐋 Installing .NET 🐋\t\n"
 # prerequisites for native build
 # note: change libicu70 -> libicu71 as soon as available
-apt-get install libc6  libgcc1  libgcc-s1  libgssapi-krb5-2  libicu-dev  libicu70  liblttng-ust1  libssl3  libstdc++6  libunwind8  zlib1g
+#apt-get install libc6  libgcc1  libgcc-s1  libgssapi-krb5-2  libicu-dev  libicu70  liblttng-ust1  libssl3  libstdc++6  libunwind8  zlib1g
 
 curl -LO https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.sh
 bash ./dotnet-install.sh --install-dir /opt/hostedtoolcache/dotnet --no-path --channel STS  # net 7.0
