32838546a1e67d3ba93f9a38042e3d7680d3fecb
Chris Helmich <kagekirin@gmail.com>
2023-03-27 19:29:10 +0200

    refactor (mytools.sh): remove NVM installation part

--- linux/ubuntu/scripts/mytools.sh
+++ linux/ubuntu/scripts/mytools.sh
@@ -5,24 +5,6 @@ set -Eeuo pipefail
 
 . /etc/environment
 
-printf "\n\t🐋 Installing NVM tools 🐋\t\n"
-VERSION=$(curl -s https://api.github.com/repos/nvm-sh/nvm/releases/latest | jq -r '.tag_name')
-curl -o- "https://raw.githubusercontent.com/nvm-sh/nvm/$VERSION/install.sh" | bash
-export NVM_DIR=$HOME/.nvm
-echo "NVM_DIR=$HOME/.nvm" | tee -a /etc/environment
-
-# Expressions don't expand in single quotes, use double quotes for that.shellcheck(SC2016)
-# shellcheck disable=SC2016
-echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' | tee -a /etc/skel/.bash_profile
-
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-printf "\n\t🐋 Installed NVM 🐋\t\n"
-nvm --version
-
-# node 12 and 16 are installed already in act-*
-versions=("14")
-
 printf "\n\t🐋 Installing METAGEN 🐋\t\n"
 npm install -g https://github.com/kagekirin/metagen-js/tarball/main
 which metagen
